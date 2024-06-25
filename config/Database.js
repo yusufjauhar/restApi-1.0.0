941708532e620bc8dc002d8ad003c778777ddad9
yusufjauhar <115067335+yusufjauhar@users.noreply.github.com>
2024-06-25 15:18:36 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,9 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 
-const db = new Sequelize('upload_db','root','',{
-    host: 'localhost',
-    dialect: "mysql"
+const db = new Sequelize('railway', 'root', 'kSZPjBsXCZQaArKcLkEfqGdPUmjHbeeC', {
+  host: 'monorail.proxy.rlwy.net',
+  port: 12922,
+  dialect: "mysql"
 });
 
 export default db;
